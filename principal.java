3bc827d49e8b676d61301c9f371aed334548012e
unknown <fbotnerbakovic@uade.edu.ar>
2024-08-13 11:00:50 -0300

    Rama creacion

--- principal.java
+++ principal.java
@@ -3,8 +3,8 @@
 public class principal {
         
     public static void main (String args[]){
-        System.out.println("Hola a todos");
-	System.out.println("chau");
+        System.out.println("Primer rama");
+
 
         }
 }
