a349aa0d1be2dced76e97deebbccc2ea373e8a33
Luis Ortiz <luiortiz@uade.edu.ar>
2024-08-13 17:00:52 -0300

    Update main
    le meti otro comentario rey

--- principal.java
+++ principal.java
@@ -2,6 +2,7 @@
 
 public class principal {
          //olaa
+         //q onda
     public static void main (String args[]){
         System.out.println("Hola a todos");
 	System.out.println("chau");
