03791928b2d98b13e30e321479045c59bef3a889
fedeuade <166051236+fedeuade@users.noreply.github.com>
2024-08-13 16:59:04 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,7 +1,7 @@
 
 
 public class principal {
-         //olaaaa
+         //olaa
     public static void main (String args[]){
         System.out.println("Hola a todos");
 	System.out.println("chau");
