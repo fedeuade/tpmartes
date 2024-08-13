96f7d368331f73d814f143a858c040869117dd91
unknown <fbotnerbakovic@uade.edu.ar>
2024-08-13 10:54:48 -0300

    Este es el segundo commit

--- principal.java
+++ principal.java
@@ -3,7 +3,8 @@
 public class principal {
         
     public static void main (String args[]){
-        System.out.println("Hola");
+        System.out.println("Hola a todos");
+	System.out.println("chau");
 
         }
 }
